20adfd69a48b3456068c2b429b2627efc26bdd3e
Your Name <you@example.com>
2026-02-25 15:13:58 +0530

    Frontend Update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const generateQuote = async (keyword) => {
-  const response = await axios.post("http://192.168.0.116:8000/generate", {
+  const response = await axios.post("http://127.0.0.1:8000/generate", {
     keyword: keyword,
   });
   return response.data.quote;
